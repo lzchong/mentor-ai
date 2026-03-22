4a9cb8fc99ac4b4dbead27c7f473d9d3d4f6f079
Chong Lee Zhen <kenclz93@gmail.com>
2026-03-21 20:53:42 -0400

    remove react and use native javascript motion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://lzchong.github.io",
   base: "/mentor-ai/",
-  integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
   },
